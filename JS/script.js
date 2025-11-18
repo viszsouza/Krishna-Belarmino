4a6f2c5c9457b7800c666478affe97bafb993c0b
vizssouza <vini.nelso@gmail.com>
2025-11-18 17:18:19 -0300

    Adicionando Animações

--- JS/script.js
+++ JS/script.js
@@ -85,3 +85,24 @@ document.getElementById("contatoForm").addEventListener("submit", function (e) {
     window.open(link, "_blank");
 });
 
+// ANIMAÇÕES GLOBAIS
+// Coletando o observador da página
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting === true) {
+            entry.target.classList.add('show')
+        }
+    })
+})
+
+// Selecionando os elementos com a classe ".hidden"
+const elements = document.querySelectorAll('.hidden')
+
+// Selecionando 1 arquivos por vez da classe ".hidden"
+elements.forEach((element) => myObserver.observe(element))
+
+// Selecionando os elementos com a classe ".hidden"
+const elements2 = document.querySelectorAll('.hidden2')
+
+// Selecionando 1 arquivos por vez da classe ".hidden"
+elements2.forEach((element) => myObserver.observe(element))
